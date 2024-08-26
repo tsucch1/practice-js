61f892ae7c6df7f031b15f70c2293d588ed355f5
tsuchida <tsuchida.shimadai@gmail.com>
2024-08-26 18:15:04 +0900

    0826変数・定数宣言

--- practice.js
+++ practice.js
@@ -1,3 +1,20 @@
+// JSの書き方
 function ohayou() {
   alert("Hello!");
 }
+
+// 構文
+var a = 3;
+let b = 3;
+{
+  var a = 5;
+  let b = 5;
+}
+console.log(a);
+console.log(b);
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+// 条件分岐から
