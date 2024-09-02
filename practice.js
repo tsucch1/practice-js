12ac540dc15ed747066f19acba02a97064207180
tsuchida <tsuchida.shimadai@gmail.com>
2024-09-02 21:29:50 +0900

    0902繰り返し

--- practice.js
+++ practice.js
@@ -22,22 +22,89 @@ console.log(c);
 
 // 条件分岐
 
-n = 5;
-if (n < 10) {
-  alert("Small");
-} else {
-  alert("Big");
+// n = 5;
+// if (n < 10) {
+//   alert("Small");
+// } else {
+//   alert("Big");
+// }
+
+// if (n < 10) {
+//   alert("Small");
+// }
+
+// n = 30;
+// if (n < 10) {
+//   alert("Small");
+// } else if (n > 20) {
+//   alert("Big");
+// } else {
+//   alert("Normal");
+// }
+
+// n = 2;
+// switch (n) {
+//   case 1:
+//     alert("One");
+//     break;
+//   case 2:
+//     alert("Two");
+//     break;
+//   case 3:
+//   case 4:
+//     alert("Three or Four");
+//     break;
+//   default:
+//     alert("Other");
+//     break;
+// }
+
+// x = 1;
+// switch (x) {
+//   case 1:
+//     alert("One");
+//   case 2:
+//     alert("Two");
+//   default:
+//     alert("Other");
+// }
+
+// 繰り返し
+n = 0;
+while (0 < 10) {
+  console.log(n);
+  n++;
 }
 
-if (n < 10) {
-  alert("Small");
+n = 0;
+do {
+  console.log(n);
+  n++;
+} while (n < 10);
+
+for (i = 0; i < 10; i++) {
+  console.log(i);
 }
 
-n = 30;
-if (n < 10) {
-  alert("Small");
-} else if (n > 20) {
-  alert("Big");
-} else {
-  alert("Normal");
+xx = ["Blue", "Red", "Yellow"];
+for (i in xx) {
+  console.log(xx[i]);
 }
+
+var data = { namae: "Yamada", age: 26, country: "Japan" };
+for (var d in data) {
+  console.log(d, data[d]);
+}
+
+for (var attr in navigator) {
+  console, log(attr);
+}
+
+var set = new Set();
+set.add("Tanaka");
+set.add("Suzuki");
+for (var value of set) {
+  console.log(value);
+}
+
+// ループを抜ける
