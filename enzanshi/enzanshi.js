125804d9cddd946c7192eb07cfafd5631de5ea5a
tsuchida <tsuchida.shimadai@gmail.com>
2024-09-30 21:39:36 +0900

    0930演算子終了・数値等のファイル作成

--- enzanshi/enzanshi.js
+++ enzanshi/enzanshi.js
@@ -88,3 +88,110 @@ if (!(x11 == 5)) {
 }
 
 // 条件演算子
+
+let x12 = 7;
+y12 = x12 >= 5 ? "big" : "small";
+console.log(y12);
+
+// カンマ演算子
+
+// for (var i7 = 0, j7 = 0; i7 < 10; i7++){
+//   :
+// }
+
+// ビット演算子
+
+let x13 = 0x87654321 & 0xffff0000;
+let x14 = 0x87654321 | 0xffff0000;
+let x15 = 0xffff0000 ^ 0xff00ff00;
+let x16 = ~0x0000ffff;
+console.log(x13, x14, x15, x16);
+
+let x17 = 0x12345678 << 8;
+let x18 = 0x12345678 >> 8;
+let x19 = 0x87654321 >> 8;
+let x20 = 0x87654321 >>> 8;
+console.log(x17, x18, x19, x20);
+
+// 複合代入演算子
+
+let x21 =
+  (x22 =
+  x23 =
+  x24 =
+  x25 =
+  x26 =
+  x27 =
+  x28 =
+  x29 =
+  x30 =
+  x31 =
+  x32 =
+    10);
+x21 += 5;
+x22 -= 5;
+x23 *= 5;
+x24 /= 5;
+x25 %= 5;
+x26 **= 5;
+x27 &= 0xff;
+x28 |= 0xff;
+x29 ^= 0xff;
+x30 <<= 8;
+x31 >>= 8;
+x32 >>>= 8;
+console.log(x21, x22, x23, x24, x25, x26);
+console.log(x27, x28, x29, x30, x31, x32);
+
+// 論理代入演算子
+
+let x33 = (x34 = x35 = 10);
+x33 ||= 5; // よくわからない
+x34 &&= 5;
+x35 ??= 5; // よくわからない
+console.log(x33, x34, x35);
+
+// ヌル値関連演算子
+
+// obj.foo = obj.foo ?? 60;
+// console.log(obj.foo);
+
+// console.log(response.body?.length);
+
+// その他の演算子
+
+var obj = {
+  name: "Yamada",
+  age: 24,
+};
+console.log(obj.name);
+delete obj.name;
+console.log(obj.name);
+
+// typeof undefined;
+// typeof null;
+
+void function func() {
+  console.log("Hello!");
+};
+// 何も表示されない
+
+if ("appName" in navigator) {
+  console.log("Yes");
+}
+
+p1 = {
+  name2: "Yamada",
+  age: 26,
+};
+if ("name2" in p1) {
+  console.log("Yes");
+}
+
+function Person() {}
+var p2 = new Person();
+if (p2 instanceof Person) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
